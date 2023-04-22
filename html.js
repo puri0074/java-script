a895108d438e9f48093247d395415402c26fe8c9
puri0074 <1472315@senati.pe>
2023-04-22 08:43:12 -0500

    agregando var comcentracio n js

--- html.js
+++ html.js
@@ -1,6 +1,17 @@
 alert("Faltan 23 dias dias una gran fecha!!");
 
-var nombre = "puri eduardo"
-var altura = "190"
+var texto = "¿cuantos dias falta para el gan di de la maadre"
+var texto2 ="!! un dia muy maravilloso¡¡";
 
 var concentracion = nombre + " " + altura;
+
+var datos = document.getElementByIt("datos")
+datos.inner.HTML = concentracion;
+
+var saludo = "Bienvenido"
+var nombre = "puri"
+
+var concentracion = saludo + " " + nombre;
+
+var hola = document.getElementById("hello");
+hola.inner.HTML = concentracion;
